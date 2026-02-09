7f32dc88c309fb479bbf4ac8f46c4a44c5cc37b8
Jake Spain <jake.spain@ltnglobal.com>
2026-02-09 06:22:52 -0500

    Backport upstream rewrite_non_www_to_www
    https://github.com/voxpupuli/puppet-nginx/pull/1326

--- manifests/resource/server.pp
+++ manifests/resource/server.pp
@@ -227,6 +227,7 @@ define nginx::resource::server (
   Array[String] $server_name                                                     = [$name],
   Optional[String] $www_root                                                     = undef,
   Boolean $rewrite_www_to_non_www                                                = false,
+  Boolean $rewrite_non_www_to_www                                                = false,
   Optional[Hash] $location_custom_cfg                                            = undef,
   Optional[Hash] $location_cfg_prepend                                           = undef,
   Optional[Hash] $location_cfg_append                                            = undef,
@@ -276,6 +277,10 @@ define nginx::resource::server (
 
   if ! defined(Class['nginx']) {
     fail('You must include the nginx base class before using any defined resources')
+  }
+
+  if $rewrite_www_to_non_www == true and $rewrite_non_www_to_www == true {
+    fail('You must not set both $rewrite_www_to_non_www and $rewrite_non_www_to_www to true')
   }
 
   # Variables
